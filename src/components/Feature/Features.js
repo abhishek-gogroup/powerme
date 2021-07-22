516734ec9531529395476d60b01b4cbfff6ef585
Abhishek Khatri <abhishek.khatri@gogroup.tech>
2021-07-22 13:31:31 +0530

    Features section styling changes and svgs changed.

--- src/components/Feature/Features.js
+++ src/components/Feature/Features.js
@@ -14,12 +14,12 @@ function Features() {
                         <p>Da uns die Welt am Herzen liegt, haben wir es uns zur Aufgabe gemacht, die Energiebranche zu revolutionieren. </p>
                     </Card>
                     <Card className="feature-card">
-                        <img src="https://uploads-ssl.webflow.com/6088a1db62977b0a54cdaf23/60d34510bab693534af71039_Group.svg" alt="Feature" className="feature-icon" />
+                        <img src="https://uploads-ssl.webflow.com/6088a1db62977b0a54cdaf23/60d34511d96be5d53f0a2854_Vector.svg" alt="Feature" className="feature-icon" />
                         <h6 className="feature-heading">Modern</h6>
                         <p>Da für uns Energie und Fortschritt kein Widerspruch ist, versprechen wir statt Papierkram 100% digitalen Strom. </p>
                     </Card>
                     <Card className="feature-card">
-                        <img src="https://uploads-ssl.webflow.com/6088a1db62977b0a54cdaf23/60d34510bab693534af71039_Group.svg" alt="Feature" className="feature-icon" />
+                        <img src="https://uploads-ssl.webflow.com/6088a1db62977b0a54cdaf23/60d345112a354d316deaea59_Group-1.svg" alt="Feature" className="feature-icon" />
                         <h6 className="feature-heading">Transparent</h6>
                         <p>Da leere Versprechen nicht mit unseren Grundsätzen übereinstimmen, setzen wir auf offene und faire Verträge. </p>
                     </Card>
